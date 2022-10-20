d4a3b4eb9c2af103d24cc4977193482d47d87f8b
vdeq79 <xszh79@outlook.com>
2022-10-20 12:59:47 +0200

    Avanza la tarea #9 añadiendo la clase ModeloFabricacion

---
+++ models/modelo_fabricacion.ts
@@ -0,0 +1,18 @@
+import {Modelo} from "./modelo.ts"
+
+//Objeto valor intermedio asociando cada modelo con las unidades que deben fabricarse
+export class ModeloFabricacion{
+
+    //Modelo asociado
+    modelo: Modelo;
+
+    //Cantidad asociado al modelo
+    cantidad: number;
+
+    constructor(modeloParametro:Modelo, cantidadParametro:number){
+        this.modelo = modeloParametro;
+        this.cantidad = cantidadParametro;
+    }
+
+
+}
